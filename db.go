0a5d4b7f2f125a34e89a00095bd78cca43f3ec47
Artem Redkin <artemredkin@yandex-team.ru>
2015-03-30 16:49:07 +0300

    refactor map

--- db.go
+++ db.go
@@ -1,161 +1,161 @@
 package heliosphere
 
 import (
-    "time"
-    "errors"
+	"errors"
+	"time"
 
-    "appengine"
-    "appengine/datastore"
+	"appengine"
+	"appengine/datastore"
 )
 
 type Counter struct {
-    Count int
+	Count int
 }
 
 func NewEvent(c appengine.Context, etype EventType, date time.Time, comments string) (*Event, error) {
-    id, err := Increment(c, "Event")
-    if err != nil {
-        return nil, err
-    }
+	id, err := Increment(c, "Event")
+	if err != nil {
+		return nil, err
+	}
 
-    event := Event{
-        Id: id,
-        Type: etype,
-        Date: date,
-        Comments: comments,
-    }
+	event := Event{
+		Id:       id,
+		Type:     etype,
+		Date:     date,
+		Comments: comments,
+	}
 
-    key := datastore.NewIncompleteKey(c, "Event", nil)
-    _, err = datastore.Put(c, key, &event)
+	key := datastore.NewIncompleteKey(c, "Event", nil)
+	_, err = datastore.Put(c, key, &event)
 
-    return &event, err
+	return &event, err
 }
 
 func RemoveEvent(c appengine.Context, event *Event) error {
 	q := datastore.NewQuery("Event").Filter("Id =", event.Id).Limit(1).KeysOnly()
-    events, err := q.GetAll(c, nil)
-    if err != nil {
-        return err
-    }
-    if len(events) != 1 {
-        return errors.New("Event not found")
-    }
-    err = datastore.Delete(c, events[0])
-    return err
+	events, err := q.GetAll(c, nil)
+	if err != nil {
+		return err
+	}
+	if len(events) != 1 {
+		return errors.New("Event not found")
+	}
+	err = datastore.Delete(c, events[0])
+	return err
 }
 
 func GetEvent(c appengine.Context, id int) (*Event, error) {
 	q := datastore.NewQuery("Event").Filter("Id =", id).Limit(1)
 	var events []Event
-    _, err := q.GetAll(c, &events)
-    if err != nil {
-        return nil, err
-    }
-    if len(events) != 1 {
-        return nil, errors.New("Event not found")
-    }
-    return &events[0], nil
+	_, err := q.GetAll(c, &events)
+	if err != nil {
+		return nil, err
+	}
+	if len(events) != 1 {
+		return nil, errors.New("Event not found")
+	}
+	return &events[0], nil
 }
 
 func GetEvents(c appengine.Context, page int) ([]Event, error) {
 	q := datastore.NewQuery("Event").Filter("Date >=", time.Now().Add(time.Hour)).Order("Date").Limit(10).Offset(10 * (page - 1))
-    var events []Event
-    _, err := q.GetAll(c, &events)
-    return events, err
+	var events []Event
+	_, err := q.GetAll(c, &events)
+	return events, err
 }
 
 func GetPlayer(c appengine.Context, id string) (*Player, error) {
 	q := datastore.NewQuery("Player").Filter("Id =", id).Limit(1)
 	var events []Player
-    _, err := q.GetAll(c, &events)
-    if err != nil {
-        return nil, err
-    }
-    if len(events) != 1 {
-        return nil, errors.New("Player not found")
-    }
-    return &events[0], nil
+	_, err := q.GetAll(c, &events)
+	if err != nil {
+		return nil, err
+	}
+	if len(events) != 1 {
+		return nil, errors.New("Player not found")
+	}
+	return &events[0], nil
 }
 
 func AddParticipant(c appengine.Context, id int, player *Player) (*Event, error) {
 	q := datastore.NewQuery("Event").Filter("Id =", id).Limit(1)
 	var events []Event
-    keys, err := q.GetAll(c, &events)
-    if err != nil {
-        return nil, err
-    }
-    if len(events) != 1 {
-        return nil, errors.New("Event not found")
-    }
-    event := &events[0]
+	keys, err := q.GetAll(c, &events)
+	if err != nil {
+		return nil, err
+	}
+	if len(events) != 1 {
+		return nil, errors.New("Event not found")
+	}
+	event := &events[0]
 
-    event.Participants = append(event.Participants, Participant{Name: player.PSNID})
+	event.Participants = append(event.Participants, Participant{Name: player.PSNID})
 
-    _, err2 := datastore.Put(c, keys[0], event)
+	_, err2 := datastore.Put(c, keys[0], event)
 
-    return event, err2
+	return event, err2
 }
 
 func RemoveParticipant(c appengine.Context, id int, player *Player) (*Event, error) {
 	q := datastore.NewQuery("Event").Filter("Id =", id).Limit(1)
 	var events []Event
-    keys, err := q.GetAll(c, &events)
-    if err != nil {
-        return nil, err
-    }
-    if len(events) != 1 {
-        return nil, errors.New("Event not found")
-    }
-    event := &events[0]
+	keys, err := q.GetAll(c, &events)
+	if err != nil {
+		return nil, err
+	}
+	if len(events) != 1 {
+		return nil, errors.New("Event not found")
+	}
+	event := &events[0]
 
-    ps := make([]Participant, 0, len(event.Participants))
-    for _, participant := range event.Participants {
-        if participant.Name != player.PSNID {
-            ps = append(ps, participant)
-        }
-    }
-    event.Participants = ps
+	ps := make([]Participant, 0, len(event.Participants))
+	for _, participant := range event.Participants {
+		if participant.Name != player.PSNID {
+			ps = append(ps, participant)
+		}
+	}
+	event.Participants = ps
 
-    _, err2 := datastore.Put(c, keys[0], event)
+	_, err2 := datastore.Put(c, keys[0], event)
 
-    return event, err2
+	return event, err2
 }
 
 func NewPlayer(c appengine.Context, id string, first string, last string, psnid string) (*Player, error) {
-    player := Player{
-        Id: id,
-        FirstName: first,
-        LastName: last,
-        PSNID: psnid,
-    }
+	player := Player{
+		Id:        id,
+		FirstName: first,
+		LastName:  last,
+		PSNID:     psnid,
+	}
 
-    key := datastore.NewIncompleteKey(c, "Player", nil)
-    _, err := datastore.Put(c, key, &player)
+	key := datastore.NewIncompleteKey(c, "Player", nil)
+	_, err := datastore.Put(c, key, &player)
 
-    return &player, err
+	return &player, err
 }
 
 func inc(c appengine.Context, key *datastore.Key) (int, error) {
-    var x Counter
-    if err := datastore.Get(c, key, &x); err != nil && err != datastore.ErrNoSuchEntity {
-        return 0, err
-    }
-    x.Count++
-    if _, err := datastore.Put(c, key, &x); err != nil {
-        return 0, err
-    }
-    return x.Count, nil
+	var x Counter
+	if err := datastore.Get(c, key, &x); err != nil && err != datastore.ErrNoSuchEntity {
+		return 0, err
+	}
+	x.Count++
+	if _, err := datastore.Put(c, key, &x); err != nil {
+		return 0, err
+	}
+	return x.Count, nil
 }
 
 func Increment(c appengine.Context, model string) (int, error) {
-    var count int
-    err := datastore.RunInTransaction(c, func(c appengine.Context) error {
-        var err1 error
-        count, err1 = inc(c, datastore.NewKey(c, "Counter", model, 0, nil))
-        return err1
-    }, nil)
-    if err != nil {
-        return 0, err
-    }
-    return count, err
+	var count int
+	err := datastore.RunInTransaction(c, func(c appengine.Context) error {
+		var err1 error
+		count, err1 = inc(c, datastore.NewKey(c, "Counter", model, 0, nil))
+		return err1
+	}, nil)
+	if err != nil {
+		return 0, err
+	}
+	return count, err
 }
--- web.go
+++ web.go
@@ -1,18 +1,18 @@
 package heliosphere
 
 import (
-    "fmt"
-    "time"
-    "sort"
-    "strings"
-    "strconv"
-    "net/http"
-    "html/template"
-	
-    "github.com/gorilla/mux"
+	"fmt"
+	"html/template"
+	"net/http"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 
-    "appengine"
-    "appengine/user"
+	"github.com/gorilla/mux"
+
+	"appengine"
+	"appengine/user"
 )
 
 var tindex, _ = template.ParseFiles("templates/index.html", "templates/events.html")
@@ -20,300 +20,303 @@ var tevents, _ = template.ParseFiles("templates/events.html")
 var tplayer, _ = template.ParseFiles("templates/player.html")
 
 func initWeb() {
-    router := mux.NewRouter()
-    
-    router.HandleFunc("/events", events).Methods("GET")
-    router.HandleFunc("/events", add_event).Methods("POST")
-    router.HandleFunc("/events/{id}", event).Methods("GET")
-    router.HandleFunc("/events/{id}", delete_event).Methods("DELETE")
-    router.HandleFunc("/events/{id}/_join", join_event).Methods("POST")
-    router.HandleFunc("/events/{id}/_leave", leave_event).Methods("POST")
+	router := mux.NewRouter()
 
-    router.HandleFunc("/players/new", add_player_form).Methods("GET")
-    router.HandleFunc("/players", add_player).Methods("POST")
+	router.HandleFunc("/events", events).Methods("GET")
+	router.HandleFunc("/events", add_event).Methods("POST")
+	router.HandleFunc("/events/{id}", event).Methods("GET")
+	router.HandleFunc("/events/{id}", delete_event).Methods("DELETE")
+	router.HandleFunc("/events/{id}/_join", join_event).Methods("POST")
+	router.HandleFunc("/events/{id}/_leave", leave_event).Methods("POST")
 
-    http.Handle("/", router)
+	router.HandleFunc("/players/new", add_player_form).Methods("GET")
+	router.HandleFunc("/players", add_player).Methods("POST")
+
+	http.Handle("/", router)
 }
 
 func render(w http.ResponseWriter, tmpl *template.Template, data interface{}) {
-    err := tmpl.Execute(w, data)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func event(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+	c := appengine.NewContext(r)
 
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-    event, err := GetEvent(c, id)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	event, err := GetEvent(c, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    fmt.Fprint(w, event.Type)
+	fmt.Fprint(w, event.Type)
 }
 
 func events(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 
-    player, _ := GetPlayer(c, user.Current(c).ID)
-    if player == nil {
-        http.Redirect(w, r, "/players/new", http.StatusFound)
-        return
-    }
+	player, _ := GetPlayer(c, user.Current(c).ID)
+	if player == nil {
+		http.Redirect(w, r, "/players/new", http.StatusFound)
+		return
+	}
 
 	events, err := GetEvents(c, 1)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    grouping := make(map[string][]EventProto)
+	grouping := make(map[int64]*EventsProto)
+	for _, event := range events {
+		add(&event, grouping, player)
+	}
 
-    for _, event := range events {
-        rounded := stringify(event.Date.Truncate(24 * time.Hour))
-        grouping[rounded] = append(grouping[rounded], proto(event, player))
-    }
-
-    render(w, tindex, grouping)
+	render(w, tindex, grouping)
 }
 
-func proto(event Event, player *Player) (EventProto) {
-    participating := false
-    for _, participant := range event.Participants {
-        if player.PSNID == participant.Name {
-            participating = true
-            break
-        }
-    }
-    can_join := !participating && event.HasRoom()
-    return EventProto{Event: &event, IsParticipant: participating, CanJoin: can_join}
+func proto(event *Event, player *Player) *EventProto {
+	participating := false
+	for _, participant := range event.Participants {
+		if player.PSNID == participant.Name {
+			participating = true
+			break
+		}
+	}
+	can_join := !participating && event.HasRoom()
+	return &EventProto{Event: *event, IsParticipant: participating, CanJoin: can_join}
+}
+
+func add(event *Event, grouping map[int64]*EventsProto, player *Player) {
+	proto := proto(event, player)
+	rounded_time := event.Date.Truncate(24 * time.Hour)
+	rounded_msec := rounded_time.Unix()
+	_, in := grouping[rounded_msec]
+	if !in {
+		grouping[rounded_msec] = &EventsProto{Date: stringify(rounded_time)}
+	}
+	grouping[rounded_msec].append(proto)
 }
 
 func stringify(t time.Time) string {
-    const layout = "January, 2"
-    today := time.Now().Truncate(24 * time.Hour)
-    if t.UnixNano() == today.UnixNano() {
-        return "Today"
-    } else if t.UnixNano() == today.Add(24 * time.Hour).UnixNano() {
-        return "Tomorrow"
-    } else {
-        return t.Format(layout)
-    }
+	const layout = "January, 2"
+	today := time.Now().Truncate(24 * time.Hour)
+	if t.UnixNano() == today.UnixNano() {
+		return "Today"
+	} else if t.UnixNano() == today.Add(24*time.Hour).UnixNano() {
+		return "Tomorrow"
+	} else {
+		return t.Format(layout)
+	}
 }
 
 func add_event(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+	c := appengine.NewContext(r)
 
-    player, _ := GetPlayer(c, user.Current(c).ID)
+	player, _ := GetPlayer(c, user.Current(c).ID)
 
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    etype, err2 := ParseType(strings.TrimSpace(r.PostFormValue("etype")))
-    if err2 != nil {
-        http.Error(w, err2.Error(), http.StatusBadRequest)
-        return
-    }
+	etype, err2 := ParseType(strings.TrimSpace(r.PostFormValue("etype")))
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusBadRequest)
+		return
+	}
 
-    edate := strings.TrimSpace(r.PostFormValue("date"))
-    etime := strings.TrimSpace(r.PostFormValue("time"))
+	edate := strings.TrimSpace(r.PostFormValue("date"))
+	etime := strings.TrimSpace(r.PostFormValue("time"))
 
-    const layout = "01/02/2006 15:04"
-    date, err3 := time.Parse(layout, edate + " " + etime)
-    if err3 != nil {
-        http.Error(w, err3.Error(), http.StatusBadRequest)
-        return
-    }
+	const layout = "01/02/2006 15:04"
+	date, err3 := time.Parse(layout, edate+" "+etime)
+	if err3 != nil {
+		http.Error(w, err3.Error(), http.StatusBadRequest)
+		return
+	}
 
-    comments := strings.TrimSpace(r.PostFormValue("comments"))
+	comments := strings.TrimSpace(r.PostFormValue("comments"))
 
-    event, err4 := NewEvent(c, etype, date, comments)
-    if err4 != nil {
-        http.Error(w, err4.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    events, err5 := GetEvents(c, 1)
-    if err5 != nil {
-        http.Error(w, err4.Error(), http.StatusInternalServerError)
-        return
-    }
+	event, err4 := NewEvent(c, etype, date, comments)
+	if err4 != nil {
+		http.Error(w, err4.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    events = append(events, *event)
-    sort.Sort(ByDate(events))
+	events, err5 := GetEvents(c, 1)
+	if err5 != nil {
+		http.Error(w, err4.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    grouping := make(map[string][]EventProto)
-    for _, event := range events {
-        rounded := stringify(event.Date.Truncate(24 * time.Hour))
-        grouping[rounded] = append(grouping[rounded], proto(event, player))
-    }
+	events = append(events, *event)
+	sort.Sort(ByDate(events))
 
-    render(w, tevents, grouping)
+	grouping := make(map[int64]*EventsProto)
+	for _, event := range events {
+		add(&event, grouping, player)
+	}
+
+	render(w, tevents, grouping)
 }
 
 func delete_event(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+	c := appengine.NewContext(r)
 
-    player, _ := GetPlayer(c, user.Current(c).ID)
+	player, _ := GetPlayer(c, user.Current(c).ID)
 
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-    to_delete, err2 := GetEvent(c, id)
-    if err2 != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    err3 := RemoveEvent(c, to_delete)
-    if err3 != nil {
-        http.Error(w, err3.Error(), http.StatusInternalServerError)
-        return
-    }
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	to_delete, err2 := GetEvent(c, id)
+	if err2 != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    events, err4 := GetEvents(c, 1)
-    if err != nil {
-        http.Error(w, err4.Error(), http.StatusInternalServerError)
-        return
-    }
+	err3 := RemoveEvent(c, to_delete)
+	if err3 != nil {
+		http.Error(w, err3.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    sort.Sort(ByDate(events))
+	events, err4 := GetEvents(c, 1)
+	if err != nil {
+		http.Error(w, err4.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    grouping := make(map[string][]EventProto)
-    for _, event := range events {
-        if event.Id != to_delete.Id {
-            rounded := stringify(event.Date.Truncate(24 * time.Hour))
-            grouping[rounded] = append(grouping[rounded], proto(event, player))
-        }
-    }
+	sort.Sort(ByDate(events))
 
-    render(w, tevents, grouping)
+	grouping := make(map[int64]*EventsProto)
+	for _, event := range events {
+		if event.Id != to_delete.Id {
+			add(&event, grouping, player)
+		}
+	}
+
+	render(w, tevents, grouping)
 }
 
 func join_event(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+	c := appengine.NewContext(r)
 
-    player, _ := GetPlayer(c, user.Current(c).ID)
+	player, _ := GetPlayer(c, user.Current(c).ID)
 
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    event, err := AddParticipant(c, id, player)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	event, err := AddParticipant(c, id, player)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    events, err2 := GetEvents(c, 1)
-    if err2 != nil {
-        http.Error(w, err2.Error(), http.StatusInternalServerError)
-        return
-    }
+	events, err2 := GetEvents(c, 1)
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    grouping := make(map[string][]EventProto)
-    for _, e := range events {
-        if e.Id == event.Id {
-            rounded := stringify(event.Date.Truncate(24 * time.Hour))
-            grouping[rounded] = append(grouping[rounded], proto(*event, player))
-        } else {
-            rounded := stringify(e.Date.Truncate(24 * time.Hour))
-            grouping[rounded] = append(grouping[rounded], proto(e, player))
-        }
-    }
+	grouping := make(map[int64]*EventsProto)
+	for _, e := range events {
+		if e.Id == event.Id {
+			add(event, grouping, player)
+		} else {
+			add(&e, grouping, player)
+		}
+	}
 
-    render(w, tevents, grouping)
+	render(w, tevents, grouping)
 }
 
 func leave_event(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+	c := appengine.NewContext(r)
 
-    player, _ := GetPlayer(c, user.Current(c).ID)
+	player, _ := GetPlayer(c, user.Current(c).ID)
 
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    event, err := RemoveParticipant(c, id, player)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	event, err := RemoveParticipant(c, id, player)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    events, err2 := GetEvents(c, 1)
-    if err2 != nil {
-        http.Error(w, err2.Error(), http.StatusInternalServerError)
-        return
-    }
+	events, err2 := GetEvents(c, 1)
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    grouping := make(map[string][]EventProto)
-    for _, e := range events {
-        if e.Id == event.Id {
-            rounded := stringify(event.Date.Truncate(24 * time.Hour))
-            grouping[rounded] = append(grouping[rounded], proto(*event, player))
-        } else {
-            rounded := stringify(e.Date.Truncate(24 * time.Hour))
-            grouping[rounded] = append(grouping[rounded], proto(e, player))
-        }
-    }
+	grouping := make(map[int64]*EventsProto)
+	for _, e := range events {
+		if e.Id == event.Id {
+			add(event, grouping, player)
+		} else {
+			add(&e, grouping, player)
+		}
+	}
 
-    render(w, tevents, grouping)
+	render(w, tevents, grouping)
 }
 
 func add_player_form(w http.ResponseWriter, r *http.Request) {
-    render(w, tplayer, nil)
+	render(w, tplayer, nil)
 }
 
 func add_player(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
+	c := appengine.NewContext(r)
 
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    first := strings.TrimSpace(r.PostFormValue("first"))
-    last := strings.TrimSpace(r.PostFormValue("last"))
-    psnid := strings.TrimSpace(r.PostFormValue("psnid"))
+	first := strings.TrimSpace(r.PostFormValue("first"))
+	last := strings.TrimSpace(r.PostFormValue("last"))
+	psnid := strings.TrimSpace(r.PostFormValue("psnid"))
 
-    _, err2 := NewPlayer(c, user.Current(c).ID, first, last, psnid)
-    if err2 != nil {
-        http.Error(w, err2.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/events", http.StatusFound)
+	_, err2 := NewPlayer(c, user.Current(c).ID, first, last, psnid)
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/events", http.StatusFound)
 }
